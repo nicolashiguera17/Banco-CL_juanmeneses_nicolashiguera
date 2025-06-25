f0700ce11b715be51683343f3f13d99eb84ad971
JDMeneses27 <189859220+JDMeneses27@users.noreply.github.com.>
2025-06-25 07:12:16 -0500

    feat: :sparkles: Hicimos los primeros 13 datos de la tabla de clientes

---
+++ dml.sql
@@ -0,0 +1,17 @@
+-- TABLA USUARIOS
+
+INSERT INTO Clientes (id_cliente, nombre, numero_cuenta, telefono, correo, id_empleado) VALUES
+(1, 'Camila Hernández', '1293847583', '+57 3114567890', 'camilahernandez@gmail.com', 5),
+(2, 'David Torres', '2309485761', '+57 3167891234', 'davidtorres@gmail.com', 3),
+(3, 'Luisa Fernanda Gutiérrez', '9876543210', '+57 3109876543', 'luisafernandag@mail.com', 7),
+(4, 'Andrés López', '5647382910', '+57 3151234567', 'andreslopez@gmail.com', 2),
+(5, 'Sofía Pérez', '8473629102', '+57 3147894561', 'sofiaperez@gmail.com', 1),
+(6, 'Juan Camilo Rodríguez', '3948571028', '+57 3196543210', 'juancamilor@gmail.com', 9),
+(7, 'Valentina Rojas', '2983746501', '+57 3123456789', 'valentinarojas@gmail.com', 4),
+(8, 'Carlos Eduardo Nieto', '8374659201', '+57 3104567891', 'carlosnieto@gmail.com', 6),
+(9, 'Mariana Arias', '5647389201', '+57 3145678902', 'marianaarias@gmail.com', 11),
+(10, 'Daniel Esteban Ruiz', '1092837465', '+57 3171234567', 'danielruiz@gmail.com', 8),
+(11, 'Tatiana Forero', '3847561920', '+57 3134567890', 'tatianaforero@gmail.com', 13),
+(12, 'Santiago León', '9081726345', '+57 3187890123', 'santiagoleon@gmail.com', 14),
+(13, 'Isabela Martínez', '6574839201', '+57 3107894561', 'isabelamartinez@gmail.com', 15),
+
